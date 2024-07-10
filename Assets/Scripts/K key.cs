d48761f2dd36eeac55ca0ea0ad60d7662eb9c0bd
Henderson The Coder School <68570781+hendersoncoderschool@users.noreply.github.com>
2024-07-10 16:01:37 -0700

    day 2

--- Assets/Scripts/K key.cs
+++ Assets/Scripts/K key.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HitNote : MonoBehaviour
+public class Kkey : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,8 @@ public class HitNote : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("k")) {
+            Destroy(gameObject);
+        }
     }
 }
