0c8a6a1e81ec2c257ff60e8151ecc3b3872c376a
Henderson The Coder School <68570781+hendersoncoderschool@users.noreply.github.com>
2024-07-17 17:03:15 -0700

    note input timing

--- Assets/Scripts/Keys.cs
+++ Assets/Scripts/Keys.cs
@@ -6,30 +6,35 @@ public class Keys : MonoBehaviour
 {
     public string key;
     int score;
+    bool entered;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        entered = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(key)) {
-            //Destroy(gameObject);
-        }
+        transform.Translate(Vector2.down * Time.deltaTime * 10);
         if (transform.position.y < -5) {
+            Destroy(gameObject);
+        }
+        if (Input.GetKeyDown(key) && entered) {
+            score += 1;
+            print("hello");
             Destroy(gameObject);
         }
     }
 
     void OnTriggerEnter2D(Collider2D col) {
-        Debug.Log("collide");
-        if (Input.GetKeyDown(key)) {
-            score += 1;
-            print("hello");
-            Destroy(gameObject);
-        }
+        //Debug.Log("collide");
+        entered = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        //Debug.Log("collide");
+        entered = false;
     }
 }
